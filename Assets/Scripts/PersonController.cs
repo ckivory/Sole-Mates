1bace78f54ddb76e4310c724250dc6b2c69de4c9
ckivory <ckivory@gmail.com>
2021-04-08 21:08:26 -0600

    Can now spawn multiple people with random qualities, requirements, and dealbreakers. Added textbox on hover.

--- Assets/Scripts/PersonController.cs
+++ Assets/Scripts/PersonController.cs
@@ -2,33 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class PersonController : MonoBehaviour
+public class PersonController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public static int maxFontSize = 40;
+    public static int fontMargin = 16;
+
+    public Image bodyImage;
+    public Image infoImage;
+
+    [HideInInspector]
+    public Text qualitiesText;
+    [HideInInspector]
+    public Text requirementText;
+    [HideInInspector]
+    public Text dealbreakerText;
+
+    public Color bodyColor;
 
     public List<char> qualities;
     public List<char> requirements;
     public List<char> dealbreakers;
 
-    public Text qualitiesText;
-
-    void Start()
+    public void InitializePerson()
     {
-        updateQualitiesText();
+        InitializeText();
+        InitializeColor();
+        infoImage.gameObject.SetActive(false);
     }
 
-    public void updateQualitiesText()
+    public void InitializeText()
     {
-        qualitiesText.text = "";
-        for (int i = 0; i < qualities.Count - 1; i++)
-        {
-            char q = qualities[i];
-            qualitiesText.text += q + " ";
-        }
-        qualitiesText.text += qualities[qualities.Count - 1];
-
+        qualitiesText = bodyImage.gameObject.transform.GetComponentInChildren<Text>();
+        requirementText = infoImage.gameObject.transform.GetChild(0).GetComponent<Text>();
+        dealbreakerText = infoImage.gameObject.transform.GetChild(1).GetComponent<Text>();
+        updateText(qualitiesText, qualities);
         resizeQualities();
+        updateInfoBox();
+    }
+
+    public void InitializeColor()
+    {
+        bodyImage.color = bodyColor;
+    }
+
+    public void updateText(Text tc, List<char> letters)
+    {
+        tc.text = "";
+        for (int i = 0; i < letters.Count - 1; i++)
+        {
+            char q = letters[i];
+            tc.text += q + " ";
+        }
+        if(letters.Count > 0)
+        {
+            tc.text += letters[letters.Count - 1];
+        }
+    }
+
+    public void updateInfoBox()
+    {
+        updateText(requirementText, requirements);
+        updateText(dealbreakerText, dealbreakers);
+        if(requirements.Count < 1)
+        {
+            dealbreakerText.transform.localPosition = Vector2.zero;
+        }
+        if(dealbreakers.Count > 0)
+        {
+            dealbreakerText.text = "¬(" + dealbreakerText.text + ")";
+        } else
+        {
+            requirementText.transform.localPosition = Vector2.zero;
+        }
+        
+        float newWidth = Mathf.Max(requirementText.preferredWidth, dealbreakerText.preferredWidth) + 16;
+        infoImage.rectTransform.sizeDelta = new Vector2(newWidth, infoImage.rectTransform.sizeDelta.y);
     }
 
     public void resizeQualities()
@@ -38,5 +88,19 @@ public class PersonController : MonoBehaviour
         {
             qualitiesText.fontSize -= 1;
         }
+    }
+
+    // Figure out how to make it only work when you hover over the body, not any other part. Maybe attach a script to the body itself.
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Entering");
+        transform.SetAsLastSibling();
+        infoImage.gameObject.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Exiting");
+        infoImage.gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/SimulationManager.cs
+++ Assets/Scripts/SimulationManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class SimulationManager : MonoBehaviour
 {
+    public Canvas canvas;
+    public GameObject person;
+
     public uint N;   // Number of people in simulation. Constrained to be even
     [Range(1, 26)]
     public uint U;   // Unique qualities to choose from. Represented by letters. Cannot exceed 26.
@@ -11,29 +14,144 @@ public class SimulationManager : MonoBehaviour
     public uint R;   // Requirements per person. Optimality of a match is determined by (r-d)/R, where r and d are the number of qualities from R and D present in a potential partner.
     public uint D;   // Dealbreakers per person. Cannot share qualities with R. R + D cannot exceed Q.
 
+    public static char[] alphabet = new char[26] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
     // For now, just verify that qualities do not contradict. Eventually, allow player to choose qualities.
     public void Start()
     {
-        if(N % 2 != 0)
+        if (Verify())
+        {
+            Populate();
+        }
+        else
+        {
+            Debug.Log("Exiting.");
+        }
+    }
+
+    public bool Verify()
+    {
+        if (N % 2 != 0)
         {
             Debug.Log("N must be an even number.");
+            return false;
         }
-        else if(U < 1 || U > 26)
+        else if (U < 1 || U > 26)
         {
             Debug.Log("U must be between 1 and 26 inclusive.");
+            return false;
         }
         else if (Q > U)
         {
             Debug.Log("Q cannot exceed U.");
+            return false;
         }
-        else if(R + D > Q)
+        else if (R + D > U)
         {
-            Debug.Log("R + D cannot exceed Q.");
+            Debug.Log("R + D cannot exceed U.");
+            return false;
         }
         else
         {
-            Debug.Log("Ready to begin populating simulation!");
+            return true;
         }
     }
 
+    public void Populate()
+    {
+        uint radius = 150;  // Might calculate at runtime later
+
+        for (int i = 0; i < N; i++)
+        {
+            float angle = ((float)i / N) * (2 * Mathf.PI);
+            CreatePerson(new Vector2(Mathf.Sin(angle), Mathf.Cos(angle)) * radius);
+        }
+    }
+
+    public void CreatePerson(Vector2 position)
+    {
+        GameObject newPerson = Instantiate(person);
+        newPerson.transform.SetParent(canvas.transform);
+        newPerson.transform.localPosition = position;
+
+        PersonController pc = newPerson.GetComponent<PersonController>();
+
+        pc.bodyColor = randomSaturatedColor();
+        pc.qualities = randomQualities();
+        pc.requirements = randomRequirements();
+        Debug.Log("Requirements: " + string.Join(",", pc.requirements.ToArray()));
+        pc.dealbreakers = randomDealbreakers(pc.requirements);
+        Debug.Log("Dealbreakers: " + string.Join(",", pc.dealbreakers.ToArray()));
+        pc.InitializePerson();
+    }
+
+    public Color randomSaturatedColor()
+    {
+        Vector3 colorVec = new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        int minPos = 0;
+        int maxPos = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            colorVec[i] = Mathf.Abs(colorVec[i]);
+            if (colorVec[i] < colorVec[minPos])
+            {
+                minPos = i;
+            }
+            else if (colorVec[i] > colorVec[maxPos])
+            {
+                maxPos = i;
+            }
+        }
+        colorVec[minPos] = 0.25f;
+        colorVec[maxPos] = 1f;
+
+        return new Color(colorVec.x, colorVec.y, colorVec.z);
+    }
+
+    public List<char> randomQualities()
+    {
+        return randomSelection(Q, new List<char>());
+    }
+
+    public List<char> randomRequirements()
+    {
+        return randomSelection(R, new List<char>());
+    }
+
+    public List<char> randomDealbreakers(List<char> requirements)
+    {
+        return randomSelection(D, requirements);
+    }
+
+    public List<char> randomSelection(uint total, List<char> exclude)
+    {
+        if (total > U - exclude.Count)
+        {
+            Debug.Log("Attempting to select too many qualities");
+            return new List<char>();
+        }
+
+        List<char> selection = new List<char>();
+
+        List<char> choices = new List<char>();
+        for (int i = 0; i < U; i++)
+        {
+            if(!exclude.Contains(alphabet[i]))
+            {
+                choices.Add(alphabet[i]);
+            }
+        }
+
+        for (int i = 0; i < total; i++)
+        {
+            // Because total cannot be greater than U, this should never fail
+            int choice = Random.Range(0, choices.Count);
+            char quality = choices[choice];
+            choices.RemoveAt(choice);
+            selection.Add(quality);
+        }
+
+        selection.Sort();
+        return selection;
+    }
 }
